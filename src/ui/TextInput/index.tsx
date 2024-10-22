b868fde30da4b16db5a8d18a4cce045fa32b4a39
shyptia <shyptianastya27@gmail.com>
2024-10-22 16:36:48 +0300

    add TextInput component

---
+++ src/ui/TextInput/index.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import classNames from "classnames";
+import styles from "./TextInput.module.scss";
+
+export const TextInput = ({
+  label,
+  value,
+  onChange,
+  placeholder,
+  error,
+  type = "text",
+  name,
+  onBlur,
+}: TextInputProps) => {
+  return (
+    <div className={styles.inputGroup}>
+      <label className={styles.inputLabel}>{label}</label>
+      <input
+        id={name}
+        type={type}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+        placeholder={placeholder}
+        className={classNames(styles.inputField, {
+          [styles.errorBorder]: error,
+        })}
+      />
+      {error && <p className={styles.errorMessage}>{error}</p>}
+    </div>
+  );
+};
+
+interface TextInputProps {
+  label: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+  error?: string;
+  type?: string;
+  name: string;
+  onBlur: (e: React.FocusEvent<HTMLInputElement>) => void;
+}
