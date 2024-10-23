a48f2bdb0753464ebc3d8672d2520c47f355e75b
shyptia <shyptianastya27@gmail.com>
2024-10-23 16:02:55 +0300

    change trips page

--- src/views/Trips/index.tsx
+++ src/views/Trips/index.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import { Button } from "../../ui/Button";
 import styles from "./Trips.module.scss";
 import { useRouter } from "next/router";
+import { useLocalStorage } from "@/hooks/useLocalStorage";
+import { Trip } from "@/types";
 
 export const TripsPage = () => {
   const router = useRouter();
+  const [trips] = useLocalStorage<Trip[]>("trips", []);
 
   const handleNewTripClick = () => {
     router.push("/trips/new");
@@ -20,8 +23,23 @@ export const TripsPage = () => {
           className={styles.createTripButton}
         />
       </div>
+
       <div className={styles.content}>
-        <p>No trips yet. Start by creating a new trip.</p>
+        {trips.length > 0 ? (
+          <div>
+            <p>
+              You have {trips.length} trip(s) stored. Check them out on the
+              board!
+            </p>
+            <Button
+              text="View Trips"
+              onClick={() => router.push("/trips/board")}
+              className={styles.viewTripsButton}
+            />
+          </div>
+        ) : (
+          <p>No trips yet. Start by creating a new trip.</p>
+        )}
       </div>
     </div>
   );
