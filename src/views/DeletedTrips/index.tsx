81a1fefcd3141c8b2a1b376f2820cbf9ede94d0d
shyptia <shyptianastya27@gmail.com>
2025-01-05 16:58:10 +0200

    Add ability to restore all deleted trips

--- src/views/DeletedTrips/index.tsx
+++ src/views/DeletedTrips/index.tsx
@@ -3,9 +3,25 @@ import { Trip } from "@/types";
 import styles from "./DeletedTrips.module.scss";
 import { useSessionStorage } from "@/hooks/useSessionStorage";
 import { TripCard } from "@/components/TripCard";
+import { useLocalStorage } from "@/hooks/useLocalStorage";
+import { Button } from "@/ui";
+import { useRouter } from "next/router";
 
 export const DeletedTrips = () => {
-  const [deletedTrips] = useSessionStorage<Trip[]>("deletedTrips", []);
+  const router = useRouter();
+  const [deletedTrips, setDeletedTrips] = useSessionStorage<Trip[]>(
+    "deletedTrips",
+    []
+  );
+  const [, setTrips] = useLocalStorage<Trip[]>("trips", []);
+
+  const restoreAllTrips = () => {
+    if (deletedTrips.length > 0) {
+      setTrips((prevTrips) => [...prevTrips, ...deletedTrips]);
+      setDeletedTrips([]);
+      router.push("/board");
+    }
+  };
 
   if (!deletedTrips.length) {
     return <div className={styles.noTrips}>No deleted trips available.</div>;
@@ -13,7 +29,15 @@ export const DeletedTrips = () => {
 
   return (
     <div className={styles.deletedTrips}>
-      <h1 className={styles.title}>Deleted Trips</h1>
+      <div className={styles.header}>
+        <h1 className={styles.title}>Deleted Trips</h1>
+        <Button
+          text="Restore All Trips"
+          className={styles.restoreButton}
+          onClick={restoreAllTrips}
+          color="green"
+        />
+      </div>
       <div className={styles.cardContainer}>
         {deletedTrips.map((trip) => (
           <TripCard key={trip.id} trip={trip} isDeletedTrip />
